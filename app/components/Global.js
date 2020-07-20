fb3cb6a47bfa1b12e52f2be40fdf9bca48411886
iulianstave <iulian.staverescu@gmail.com>
2020-07-20 17:58:23 +0300

    Add index.css stylesheet

--- app/components/Global.js
+++ app/components/Global.js
@@ -3,18 +3,35 @@ import React, { Component } from "react";
 import { FormGroup, FormControl, InputGroup, Button, Glyphicon } from "react-bootstrap";
 
 class Global extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
   search() {
     console.log("search");
+    console.log(this.state.query);
   }
 
   render() {
     return (
-      <div>
+      <div className="Global">
         <h2>Book explorer</h2>
 
         <FormGroup>
           <InputGroup>
-            <FormControl type="text" placeholder="Search for a book" />
+            <FormControl 
+              type="text"
+              placeholder="Search for a book"
+              onChange={event => this.setState({query: event.target.value})}
+              onKeyPress = { event => {
+                if (event.key == 'Enter'){
+                  this.search();
+                }
+              }}
+              />
             <InputGroup.Append onClick= {()=> this.search()}>
               <Button variant="outline-secondary">Search</Button>
             </InputGroup.Append>
